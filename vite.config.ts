3b310363071767aa2c64b3522a0a05ef1aeb688c
jorgel <jorgeluisogomez@gmail.com>
2022-08-24 22:50:11 -0500

    :wrench: coverage setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'html', 'lcov'],
+    },
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
+    includeSource: ['**/*.ts']
+  },
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
   server: {
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: 8080,
+  },
 })
